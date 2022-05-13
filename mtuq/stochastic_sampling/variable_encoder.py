95a90db2480c60f9a25cc71a1f21b523ff4be9e0
thurinj <julienthurin06@gmail.com>
2022-05-13 15:37:09 -0400

    Adding moment tensor initialisation helper function.

--- mtuq/stochastic_sampling/variable_encoder.py
+++ mtuq/stochastic_sampling/variable_encoder.py
@@ -1,7 +1,40 @@
 import numpy as np
+from mtuq.util.math import to_mij, to_rtp, to_rho
+
 
 class CMAESParameters:
-    def __init__(self, name, lower_bound, upper_bound, scaling = 'linear', initial=None, repair='bounce_back', projection=None, **kwargs):
+    """ Variable encoder for the parallel_CMA_ES class.
+
+    This objects contain all the parameters and settings for each of the variable accepted by the CMA-ES method.
+
+    .. rubric:: Usage
+
+    Encode each CMA-ES variable with a metadata-like structure. This class holds the name and search parameters of each variable, as well as the potential specific scaling, initial values, boundary handling repair methods and projection function.
+
+    .. code::
+
+        variable_name = CMAESParameters(**parameters)
+
+    .. rubric:: Parameters
+
+    ``name`` (`str`): name of the variable, for bookeeping purposes
+
+    ``lower_bound`` (`float`): define the lower bound of the search space
+
+    ``upper_bound`` (`float`): define the upper bound of the search space
+
+    ``scaling`` (`str`):
+        - ``'linear'``: linear scaling between lower_bound and upper_bound
+        ..  Linear map from [0;10] to [a,b] ..
+
+        - ``'log'``: logarithmic scaling between lower_bound and upper_bound
+        ..  Logarithmic map from [0,10] to [a,b], with `a` and `b` typically defining 3 to 4 orders of magnitudes ..
+
+    ``upper_bound`` (`float`): define the upper bound of the search space
+
+    """
+    
+    def __init__(self, name, lower_bound, upper_bound, scaling = 'linear', initial=None, repair='rand_based', projection=None, **kwargs):
         self.name = name # Name of the parameter, used for printing and saving to file names etc...
         if lower_bound > upper_bound:
             raise ValueError('Lower bound is larger than the Upper Bound')
@@ -27,17 +60,42 @@ class CMAESParameters:
         else:
             self.grid = None
 
-    # def get_scaled_value(self, value): # Returns the scaled parameter values based on whether it is linear or a log scale. If grid has been defined then returns an interpolated value between two grid points.
-    #     if self.scaling == 'linear': # Linear scaling, returns a value between lower and upper bounds based on the input parameter values
-    #
-    #         scaled_value = (self.upper_bound - self.lower_bound) * ((value-1)/9)+self.lower_bound
-    #         scaled_value = linear_transform()
-    #
-    #     elif self.scaling == 'log': # Log scaling, returns a value between lower and upper bounds based on the log of parameter values
-    #
-    #         scaled_value = (self.upper_bound - self.lower_bound) * ((np.log(value)-np.log(1))/(np.log(10)-np.log(1)))+self.lower_bound
-    #
-    #     if self.grid is not None: # If a grid has been defined, then return the closest value on the grid
-    #         scaled_value = self.grid[np.argmin(abs((self.grid - scaled_value)))]
-    #
-    #     return np.round(scaled_value, 2) # Rounds the value to two decimal places and returns it
+
+def initialise_mt(depth=None, latitude=None, longitude=None):
+    # Defining the inverted parameters to initialise the CMA-ES
+    # The order expected by CMA-ES is set to 'Mw, v, w, kappa, sigma, h, depth, latitude, longitude', with depth, lat and lon being completely optional.
+
+    Mw = CMAESParameters('Mw', 4, 5, scaling='log', repair='rand_based', projection=to_rho)
+    v = CMAESParameters('v', -1/3, 1/3, scaling='linear', repair='rand_based')
+    w = CMAESParameters('w', (-3/8*np.pi), (3/8*np.pi), scaling='linear', repair='rand_based')
+    kappa = CMAESParameters('kappa', 0, 360, scaling='linear', repair='wrapping')
+    sigma = CMAESParameters('sigma', -90, 90, scaling='linear', repair='rand_based')
+    h = CMAESParameters('h', 0, 1, scaling='linear', repair='rand_based')
+
+    parameters_list = [Mw, v, w, kappa, sigma, h]
+
+    if depth:
+        if not isinstance(depth, dict):
+            raise TypeError('depth argument should be a dict')
+        else:
+            depth = CMAESParameters(**depth)
+            parameters_list += [depth]
+
+    if latitude:
+        if not isinstance(latitude, dict):
+            raise TypeError('depth argument should be a dict')
+        else:
+            latitude = CMAESParameters(**latitude)
+            parameters_list += [latitude]
+
+    if longitude:
+        if not isinstance(longitude, dict):
+            raise TypeError('depth argument should be a dict')
+        else:
+            longitude = CMAESParameters(**longitude)
+            parameters_list += [longitude]
+
+    return(parameters_list)
+
+def initialise_force(depth=None, latitude=None, longitude=None):
+    raise NotImplementedError
