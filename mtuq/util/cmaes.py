9673eabd1ba3589ddc00f64ee90fb54c434a8df3
thurinj <julienthurin06@gmail.com>
2022-05-16 17:29:58 -0400

    Added comments for all boundary constraint handling methods.

--- mtuq/util/cmaes.py
+++ mtuq/util/cmaes.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pandas as pd
 
 def linear_transform(i, a, b):
     """ Linear map suggested by N. Hansen for appropriate parameter scaling/variable encoding in CMA-ES
@@ -95,33 +96,41 @@ class Repair:
             print('Repair method not recognized')
 
     def reinitialize(self):
-        # Redraw all out of bounds values from a uniform distribution [0,10].
+        """
+        Redraw all out of bounds values from a uniform distribution [0,10].
+        """
         self.data_array[self.l_oob] = np.random.uniform(self.lower_bound,self.upper_bound, len(self.data_array[self.l_oob]))
         self.data_array[self.u_oob] = np.random.uniform(self.lower_bound,self.upper_bound, len(self.data_array[self.u_oob]))
 
     def projection(self):
-        # Project all out of bounds values to the violated bounds.
+        """
+        Project all out of bounds values to the violated bounds.
+        """
         self.data_array[self.l_oob] = self.lower_bound
         self.data_array[self.u_oob] = self.upper_bound
 
     def reflection(self):
-        # The infeasible coordinate value of the solution is reflected back from the boundary by the amount of constraint violation.
-        # This method may be call several times if the points are out of bound for more than the length of the [0,10] domain.
+        """
+        The infeasible coordinate value of the solution is reflected back from the boundary by the amount of constraint violation.
+        This method may be call several times if the points are out of bound for more than the length of the [0,10] domain.
+        """
         self.data_array[self.l_oob] = 2*self.lower_bound - self.data_array[self.l_oob]
         self.data_array[self.u_oob] = 2*self.upper_bound - self.data_array[self.u_oob]
 
     def wrapping(self):
-        # The infeasible coordinate value is shifted by the feasible interval.
+        """
+        The infeasible coordinate value is shifted by the feasible interval.
+        """
         self.data_array[self.l_oob] = self.data_array[self.l_oob] + self.upper_bound - self.lower_bound
         self.data_array[self.u_oob] = self.data_array[self.u_oob] - self.upper_bound + self.lower_bound
 
     def transformation(self):
-        # Defines the '90%' values treshold
-            # Original thersholding implementation
+        """
+        Adaptive transformation based on 90%-tile -
+        Nonlinear transform defined by R. Biedrzycki 2019, https://doi.org/10.1016/j.swevo.2019.100627
+        """
         al = np.min([(self.upper_bound - self.lower_bound)/2,(1+np.abs(self.lower_bound-2))/20])
         au = np.min([(self.upper_bound - self.lower_bound)/2,(1+np.abs(self.upper_bound))/20])
-        # al = np.min([(self.upper_bound - self.lower_bound)/2, np.abs(self.upper_bound - self.lower_bound)*0.05])
-        # au = np.min([(self.upper_bound - self.lower_bound)/2, np.abs(self.upper_bound - self.lower_bound)*0.05])
         # Create the masks for the values out of self.lower_bound - al and self.upper_bound + au bounds
         mask_1 = self.data_array > (self.upper_bound + au)
         mask_2 = self.data_array < (self.lower_bound - al)
@@ -134,7 +143,6 @@ class Repair:
         mask_4 = (self.data_array >= (self.lower_bound - al)) & (self.data_array < (self.lower_bound + al))
         mask_5 = (self.data_array > (self.upper_bound - au)) & (self.data_array <= (self.upper_bound + au))
 
-        # Nonlinear transform defined by R. Biedrzycki 2019, https://doi.org/10.1016/j.swevo.2019.100627
         # Note that reflected data are transformed according to the same principle which results in a periodic transformation
         self.data_array[mask_3] = self.data_array[mask_3]
         self.data_array[mask_4] = self.lower_bound + (self.data_array[mask_4] - (self.lower_bound-al))**2/(4*al)
@@ -142,27 +150,34 @@ class Repair:
 
 
     def projection_to_midpoint(self):
-        # Project the particles onto the domain using the midpoint method (also reffered to as the Scaled Mutant)
-        # Get the farthest out-of-bounds mutant
+        """
+        Project the particles onto the domain using the midpoint method (also reffered to as the Scaled Mutant)
+        Get the farthest out-of-bounds mutant
+        """
         largest_outlier = self.data_array[np.argmax(np.sqrt((self.data_array-(self.lower_bound+self.upper_bound)/2)**2))]
 
         alpha = np.abs(((self.lower_bound+self.upper_bound)/2)/(largest_outlier - (self.lower_bound+self.upper_bound)/2))
         self.data_array[:] = ((1-alpha)*5 + alpha*self.data_array[:])
 
     def rand_based(self):
-        # Redraw the out of bound mutants between the base vector (the CMA_ES.xmean used in draw_muants()) and the violated boundary.
-        # The base vector is the mean of the population.
-
+        """
+        Redraw the out of bound mutants between the base vector (the CMA_ES.xmean used in draw_muants()) and the violated boundary.
+        The base vector is the mean of the population.
+        """
         self.data_array[self.l_oob] = np.random.uniform(self.mean, self.lower_bound, len(self.data_array[self.l_oob]))
         self.data_array[self.u_oob] = np.random.uniform(self.mean, self.upper_bound, len(self.data_array[self.u_oob]))
 
     def midpoint_base(self):
-        # the average of the base individual and the violated constraint replaces the infeasible coordinate value.
+        """
+        The infeasible coordinate value is replaced by the midpoint between the base value and the violated boundary.
+        """
         self.data_array[self.l_oob] = (self.mean + self.lower_bound)/2
         self.data_array[self.u_oob] = (self.mean + self.upper_bound)/2
 
     def midpoint_target(self):
-        # the average of the target individual and the violated constraint replaces the infeasible coordinate value.
+        """
+        The average of the target individual and the violated constraint replaces the infeasible coordinate value.
+        """
         target = 5
         self.data_array[self.l_oob] = (target + self.lower_bound)/2
         self.data_array[self.u_oob] = (target + self.upper_bound)/2
