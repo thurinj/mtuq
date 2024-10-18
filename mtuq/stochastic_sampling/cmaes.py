c7000be0b8a37d68d78ad290e67f20457d84b79d
thurinj <63735192+thurinj@users.noreply.github.com>
2024-10-18 13:50:09 +1100

    Move plotting functions to `mtuq/stochastic_sampling/cmaes_plotting.py` and modify them to accept required parameters as arguments
    * **plot_mean_waveforms**
      - Add parameters: `mean_solution`, `final_origin`, `mode`, `callback`, `event_id`, `iteration`, `rank`
      - Replace `self.rank`, `self.mode`, `self.callback`, `self.event_id`, `self.iteration` with corresponding parameters
    * **_scatter_plot**
      - Add parameters: `mutants_logger_list`, `mode`, `fig`, `ax`, `rank`, `_datalogger`
      - Replace `self.rank`, `self.mode`, `self.fig`, `self.ax`, `self.mutants_logger_list`, `self._datalogger` with corresponding parameters

--- mtuq/stochastic_sampling/cmaes.py
+++ mtuq/stochastic_sampling/cmaes.py
@@ -5,7 +5,7 @@ from mpi4py import MPI
 import matplotlib
 matplotlib.use('Agg')
 
-from mtuq.stochastic_sampling.cmaes_utils import linear_transform, logarithmic_transform, in_bounds, array_in_bounds, Repair, inverse_linear_transform
+from mtuq.stochastic_sampling.cmaes_utils import linear_transform, logarithmic_transform, in_bounds, array_in_bounds, Repair
 from mtuq.stochastic_sampling.cmaes_plotting import plot_mean_waveforms, _scatter_plot
 from mtuq.util.math import to_mij, to_rtp, to_gamma, to_delta, wrap_180
 from mtuq import MTUQDataFrame
@@ -1019,6 +1019,174 @@ class CMA_ES(object):
                         print('Plotting results for iteration %d\n' % (i + iter_count))
                         result = self.mutants_logger_list
                         plot_misfit_force(self.event_id+'_misfit_map.png', result, colormap='viridis', backend=_plot_force_matplotlib, plot_type='colormesh', best_force=self.return_candidate_solution()[0][1::])
+
+    def plot_mean_waveforms(self, data_list, process_list, misfit_list, stations, db_or_greens_list):
+        """
+        Plots the mean waveforms using the base mtuq waveform plots (mtuq.graphics.waveforms).
+
+        Depending on the mode, different parameters are inserted into the mean solution (padding w or v with 0s for instance)
+        If green's functions a provided directly, they are used as is. Otherwise, extrace green's function from Axisem database and preprocess them.
+        Support only 1 or 2 waveform groups (body and surface waves, or surface waves only)
+
+        Arguments
+        ----------
+            data_list: A list of data to be plotted (typically `data_bw` and `data_sw`).
+            process_list: A list of processes for each data (typically `process_bw` and `process_sw`).
+            misfit_list: A list of misfits for each data (typically `misfit_bw` and `misfit_sw`).
+            stations: A list of stations.
+            db_or_greens_list: Either an AxiSEM_Client instance or a list of GreensTensors (typically `greens_bw` and `greens_sw`).
+
+        Raises
+        ----------
+            ValueError: If the mode is not 'mt', 'mt_dev', 'mt_dc', or 'force'.
+        """
+
+        if self.rank != 0:
+            return  # Exit early if not rank 0
+
+        mean_solution, final_origin = self.return_candidate_solution()
+
+        # Solution grid will change depending on the mode (mt, mt_dev, mt_dc, or force)
+        modes = {
+            'mt': ('rho', 'v', 'w', 'kappa', 'sigma', 'h'),
+            'mt_dev': ('rho', 'v', 'w', 'kappa', 'sigma', 'h'),
+            'mt_dc': ('rho', 'v', 'w', 'kappa', 'sigma', 'h'),
+            'force': ('F0', 'phi', 'h'),
+        }
+
+        if self.mode not in modes:
+            raise ValueError("Invalid mode. Supported modes for the plotting functions in the Solve method: 'mt', 'mt_dev', 'mt_dc', 'force'")
+
+        mode_dimensions = modes[self.mode]
+
+        # Pad mean_solution based on moment tensor mode (deviatoric or double couple)
+        if self.mode == 'mt_dev':
+            mean_solution = np.insert(mean_solution, 2, 0, axis=0)
+        elif self.mode == 'mt_dc':
+            mean_solution = np.insert(mean_solution, 1, 0, axis=0)
+            mean_solution = np.insert(mean_solution, 2, 0, axis=0)
+
+        solution_grid = UnstructuredGrid(dims=mode_dimensions, coords=mean_solution, callback=self.callback)
+
+        final_origin = final_origin[0]
+        if self.mode.startswith('mt'):
+            best_source = MomentTensor(solution_grid.get(0))
+        elif self.mode == 'force':
+            best_source = Force(solution_grid.get(0))
+
+        lune_dict = solution_grid.get_dict(0)
+
+        # Assignments for brevity (might be removed later)
+        data = data_list.copy()
+        process = process_list.copy()
+        misfit = misfit_list.copy()
+        greens_or_db = db_or_greens_list.copy() if isinstance(db_or_greens_list, list) else db_or_greens_list
+
+        # greens_or_db = db_or_greens_list
+
+        mode = 'db' if isinstance(greens_or_db, AxiSEM_Client) else 'greens'
+        if mode == 'db':
+            _greens = greens_or_db.get_greens_tensors(stations, final_origin)
+            greens = [None] * len(process_list)
+            for i in range(len(process_list)):
+                greens[i] = _greens.map(process_list[i])
+                greens[i][0].tags[0] = 'model:ak135f_2s'
+        elif mode == 'greens':
+            greens = greens_or_db
+
+        # Check for the occurences of mtuq.misfit.polarity.PolarityMisfit in misfit_list:
+        # if present, remove the corresponding data, greens, process and misfit from the lists
+        # Run backward to avoid index errors
+        for i in range(len(misfit)-1, -1, -1):
+            if isinstance(misfit[i], PolarityMisfit):
+                del data[i]
+                del process[i]
+                del misfit[i]
+                del greens[i]
+
+        # Plot based on the number of ProcessData objects in the process_list
+        if len(process) == 2:
+            plot_data_greens2(self.event_id + '_waveforms_mean_' + str(self.iteration) + '.png',
+                            data[0], data[1], greens[0], greens[1], process[0], process[1],
+                            misfit[0], misfit[1], stations, final_origin, best_source, lune_dict)
+        elif len(process) == 1:
+            plot_data_greens1(self.event_id + '_waveforms_mean_' + str(self.iteration) + '.png',
+                            data[0], greens[0], process[0], misfit[0], stations, final_origin, best_source, lune_dict)
+
+    def _scatter_plot(self):
+        """
+        Generates a scatter plot of the mutants and the current mean solution
+        
+        Return: 
+        Matplotlib figure object (also retrived by self.fig)
+        """
+        if self.rank == 0:
+            # Check if mode is mt, mt_dev or mt_dc or force
+            if self.mode in ['mt', 'mt_dev', 'mt_dc']:
+                if self.fig is None:  
+                    self.fig, self.ax = _generate_lune()
+
+                # Define v as by values from self.mutants_logger_list if it exists, otherwise pad with values of zeroes
+                m = np.asarray(self.mutants_logger_list['misfit'])
+
+                if 'v' in self.mutants_logger_list:
+                    v = np.asarray(self.mutants_logger_list['v'])
+                else:
+                    v = np.zeros_like(m)
+
+                if 'w' in self.mutants_logger_list:
+                    w = np.asarray(self.mutants_logger_list['w'])
+                else:
+                    w = np.zeros_like(m)
+                
+                # Handling the mean solution
+                if self.mode == 'mt':
+                    V,W = self._datalogger(mean=True)['v'], self._datalogger(mean=True)['w']
+                elif self.mode == 'mt_dev':
+                    V = self._datalogger(mean=True)['v']
+                    W = 0
+                elif self.mode == 'mt_dc':
+                    V = W = 0
+
+                # Projecting the mean solution onto the lune
+                V, W = _hammer_projection(to_gamma(V), to_delta(W))
+                self.ax.scatter(V, W, c='red', marker='x', zorder=10000000)
+                # Projecting the mutants onto the lune
+                v, w = _hammer_projection(to_gamma(v), to_delta(w))
+
+
+                vmin, vmax = np.percentile(np.asarray(m), [0,90])
+
+                self.ax.scatter(v, w, c=m, s=3, vmin=vmin, vmax=vmax, zorder=100)
+
+                self.fig.canvas.draw()
+                return self.fig
+            
+            elif self.mode == 'force':
+                if self.fig is None:
+                    self.fig, self.ax = _generate_sphere()
+
+                # phi and h will always be present in the mutants_logger_list
+                m = np.asarray(self.mutants_logger_list['misfit'])
+                phi, h = np.asarray(self.mutants_logger_list['phi']), np.asarray(self.mutants_logger_list['h'])
+                latitude = np.degrees(np.pi/2 - np.arccos(h))
+                longitude = wrap_180(phi + 90)
+                # Getting mean solution
+                PHI, H = self._datalogger(mean=True)['phi'], self._datalogger(mean=True)['h']
+                LATITUDE = np.asarray(np.degrees(np.pi/2 - np.arccos(H)))
+                LONGITUDE = wrap_180(np.asarray(PHI + 90))
+                
+                # Projecting the mean solution onto the sphere
+                LONGITUDE, LATITUDE = _hammer_projection(LONGITUDE, LATITUDE)
+                # Projecting the mutants onto the sphere
+                longitude, latitude = _hammer_projection(longitude, latitude)
+
+                vmin, vmax = np.percentile(np.asarray(m), [0,90])
+
+                self.ax.scatter(longitude, latitude, c=m, s=3, vmin=vmin, vmax=vmax, zorder=100)
+                self.ax.scatter(LONGITUDE, LATITUDE, c='red', marker='x', zorder=10000000)
+                return self.fig
+                
 
 
     def _transform_mutants(self):
--- mtuq/stochastic_sampling/cmaes_plotting.py
+++ mtuq/stochastic_sampling/cmaes_plotting.py
@@ -9,7 +9,7 @@ from mtuq.grid.force import UnstructuredGrid
 from mtuq.misfit import Misfit, PolarityMisfit
 from mtuq.util.cmaes import linear_transform, logarithmic_transform, in_bounds, array_in_bounds, Repair
 
-def plot_mean_waveforms(self, data_list, process_list, misfit_list, stations, db_or_greens_list):
+def plot_mean_waveforms(data_list, process_list, misfit_list, stations, db_or_greens_list, mean_solution, final_origin, mode, callback, event_id, iteration, rank):
     """
     Plots the mean waveforms using the base mtuq waveform plots (mtuq.graphics.waveforms).
 
@@ -24,16 +24,21 @@ def plot_mean_waveforms(self, data_list, process_list, misfit_list, stations, db
         misfit_list: A list of misfits for each data (typically `misfit_bw` and `misfit_sw`).
         stations: A list of stations.
         db_or_greens_list: Either an AxiSEM_Client instance or a list of GreensTensors (typically `greens_bw` and `greens_sw`).
+        mean_solution: The mean solution to be plotted.
+        final_origin: The final origin to be plotted.
+        mode: The mode of the inversion ('mt', 'mt_dev', 'mt_dc', or 'force').
+        callback: The callback function used for the inversion.
+        event_id: The event ID for the inversion.
+        iteration: The current iteration of the inversion.
+        rank: The rank of the current process.
 
     Raises
     ----------
         ValueError: If the mode is not 'mt', 'mt_dev', 'mt_dc', or 'force'.
     """
 
-    if self.rank != 0:
+    if rank != 0:
         return  # Exit early if not rank 0
-
-    mean_solution, final_origin = self.return_candidate_solution()
 
     # Solution grid will change depending on the mode (mt, mt_dev, mt_dc, or force)
     modes = {
@@ -43,24 +48,24 @@ def plot_mean_waveforms(self, data_list, process_list, misfit_list, stations, db
         'force': ('F0', 'phi', 'h'),
     }
 
-    if self.mode not in modes:
+    if mode not in modes:
         raise ValueError("Invalid mode. Supported modes for the plotting functions in the Solve method: 'mt', 'mt_dev', 'mt_dc', 'force'")
 
-    mode_dimensions = modes[self.mode]
+    mode_dimensions = modes[mode]
 
     # Pad mean_solution based on moment tensor mode (deviatoric or double couple)
-    if self.mode == 'mt_dev':
+    if mode == 'mt_dev':
         mean_solution = np.insert(mean_solution, 2, 0, axis=0)
-    elif self.mode == 'mt_dc':
+    elif mode == 'mt_dc':
         mean_solution = np.insert(mean_solution, 1, 0, axis=0)
         mean_solution = np.insert(mean_solution, 2, 0, axis=0)
 
-    solution_grid = UnstructuredGrid(dims=mode_dimensions, coords=mean_solution, callback=self.callback)
+    solution_grid = UnstructuredGrid(dims=mode_dimensions, coords=mean_solution, callback=callback)
 
     final_origin = final_origin[0]
-    if self.mode.startswith('mt'):
+    if mode.startswith('mt'):
         best_source = MomentTensor(solution_grid.get(0))
-    elif self.mode == 'force':
+    elif mode == 'force':
         best_source = Force(solution_grid.get(0))
 
     lune_dict = solution_grid.get_dict(0)
@@ -95,73 +100,82 @@ def plot_mean_waveforms(self, data_list, process_list, misfit_list, stations, db
 
     # Plot based on the number of ProcessData objects in the process_list
     if len(process) == 2:
-        plot_data_greens2(self.event_id + '_waveforms_mean_' + str(self.iteration) + '.png',
+        plot_data_greens2(event_id + '_waveforms_mean_' + str(iteration) + '.png',
                         data[0], data[1], greens[0], greens[1], process[0], process[1],
                         misfit[0], misfit[1], stations, final_origin, best_source, lune_dict)
     elif len(process) == 1:
-        plot_data_greens1(self.event_id + '_waveforms_mean_' + str(self.iteration) + '.png',
+        plot_data_greens1(event_id + '_waveforms_mean_' + str(iteration) + '.png',
                         data[0], greens[0], process[0], misfit[0], stations, final_origin, best_source, lune_dict)
 
-def _scatter_plot(self):
+def _scatter_plot(mutants_logger_list, mode, fig, ax, rank, _datalogger):
     """
     Generates a scatter plot of the mutants and the current mean solution
     
+    Arguments
+    ----------
+        mutants_logger_list: The list of mutants to be plotted.
+        mode: The mode of the inversion ('mt', 'mt_dev', 'mt_dc', or 'force').
+        fig: The figure object for the plot.
+        ax: The axis object for the plot.
+        rank: The rank of the current process.
+        _datalogger: The datalogger function used for the inversion.
+
     Return: 
     Matplotlib figure object (also retrived by self.fig)
     """
-    if self.rank == 0:
+    if rank == 0:
         # Check if mode is mt, mt_dev or mt_dc or force
-        if self.mode in ['mt', 'mt_dev', 'mt_dc']:
-            if self.fig is None:  
-                self.fig, self.ax = _generate_lune()
+        if mode in ['mt', 'mt_dev', 'mt_dc']:
+            if fig is None:  
+                fig, ax = _generate_lune()
 
-            # Define v as by values from self.mutants_logger_list if it exists, otherwise pad with values of zeroes
-            m = np.asarray(self.mutants_logger_list['misfit'])
+            # Define v as by values from mutants_logger_list if it exists, otherwise pad with values of zeroes
+            m = np.asarray(mutants_logger_list['misfit'])
 
-            if 'v' in self.mutants_logger_list:
-                v = np.asarray(self.mutants_logger_list['v'])
+            if 'v' in mutants_logger_list:
+                v = np.asarray(mutants_logger_list['v'])
             else:
                 v = np.zeros_like(m)
 
-            if 'w' in self.mutants_logger_list:
-                w = np.asarray(self.mutants_logger_list['w'])
+            if 'w' in mutants_logger_list:
+                w = np.asarray(mutants_logger_list['w'])
             else:
                 w = np.zeros_like(m)
             
             # Handling the mean solution
-            if self.mode == 'mt':
-                V,W = self._datalogger(mean=True)['v'], self._datalogger(mean=True)['w']
-            elif self.mode == 'mt_dev':
-                V = self._datalogger(mean=True)['v']
+            if mode == 'mt':
+                V,W = _datalogger(mean=True)['v'], _datalogger(mean=True)['w']
+            elif mode == 'mt_dev':
+                V = _datalogger(mean=True)['v']
                 W = 0
-            elif self.mode == 'mt_dc':
+            elif mode == 'mt_dc':
                 V = W = 0
 
             # Projecting the mean solution onto the lune
             V, W = _hammer_projection(to_gamma(V), to_delta(W))
-            self.ax.scatter(V, W, c='red', marker='x', zorder=10000000)
+            ax.scatter(V, W, c='red', marker='x', zorder=10000000)
             # Projecting the mutants onto the lune
             v, w = _hammer_projection(to_gamma(v), to_delta(w))
 
 
             vmin, vmax = np.percentile(np.asarray(m), [0,90])
 
-            self.ax.scatter(v, w, c=m, s=3, vmin=vmin, vmax=vmax, zorder=100)
+            ax.scatter(v, w, c=m, s=3, vmin=vmin, vmax=vmax, zorder=100)
 
-            self.fig.canvas.draw()
-            return self.fig
+            fig.canvas.draw()
+            return fig
         
-        elif self.mode == 'force':
-            if self.fig is None:
-                self.fig, self.ax = _generate_sphere()
+        elif mode == 'force':
+            if fig is None:
+                fig, ax = _generate_sphere()
 
             # phi and h will always be present in the mutants_logger_list
-            m = np.asarray(self.mutants_logger_list['misfit'])
-            phi, h = np.asarray(self.mutants_logger_list['phi']), np.asarray(self.mutants_logger_list['h'])
+            m = np.asarray(mutants_logger_list['misfit'])
+            phi, h = np.asarray(mutants_logger_list['phi']), np.asarray(mutants_logger_list['h'])
             latitude = np.degrees(np.pi/2 - np.arccos(h))
             longitude = wrap_180(phi + 90)
             # Getting mean solution
-            PHI, H = self._datalogger(mean=True)['phi'], self._datalogger(mean=True)['h']
+            PHI, H = _datalogger(mean=True)['phi'], _datalogger(mean=True)['h']
             LATITUDE = np.asarray(np.degrees(np.pi/2 - np.arccos(H)))
             LONGITUDE = wrap_180(np.asarray(PHI + 90))
             
@@ -172,6 +186,6 @@ def _scatter_plot(self):
 
             vmin, vmax = np.percentile(np.asarray(m), [0,90])
 
-            self.ax.scatter(longitude, latitude, c=m, s=3, vmin=vmin, vmax=vmax, zorder=100)
-            self.ax.scatter(LONGITUDE, LATITUDE, c='red', marker='x', zorder=10000000)
-            return self.fig
+            ax.scatter(longitude, latitude, c=m, s=3, vmin=vmin, vmax=vmax, zorder=100)
+            ax.scatter(LONGITUDE, LATITUDE, c='red', marker='x', zorder=10000000)
+            return fig
