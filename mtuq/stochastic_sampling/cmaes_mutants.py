ad3ad1756a422d6b8146da9c9735028ea7e73ac8
thurinj <julienthurin06@gmail.com>
2024-10-30 15:31:33 +1100

    Moved transform mutant to relevant file to declutter main `cmaes.py` script.

--- mtuq/stochastic_sampling/cmaes_mutants.py
+++ mtuq/stochastic_sampling/cmaes_mutants.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from mtuq.stochastic_sampling.cmaes_utils import array_in_bounds, in_bounds, Repair
+from mtuq.stochastic_sampling.cmaes_utils import array_in_bounds, in_bounds, Repair, linear_transform, logarithmic_transform
 
 
 def generate_mutants(cmaes_instance):
@@ -73,6 +73,86 @@ def apply_repair_to_param(cmaes_instance, param_values, bounds, param_idx):
         Repair(param.repair, param_values, cmaes_instance.xmean[param_idx])
     return param_values
 
+def transform_mutants(cmaes_instance):
+    """
+    Transforms local mutants on each process based on the parameters scaling and projection settings.
+
+    For each parameter, depending on its scaling setting ('linear' or 'log'), 
+    it applies a transformation to the corresponding elements of scattered_mutants.
+    If a projection is specified, it applies this projection to the transformed values.
+
+    Attributes
+    ----------
+    scattered_mutants : np.ndarray
+        A 2D numpy array with the original mutant data. When MPI is used, correspond to the local mutants on each process.
+    _parameters : list
+        A list of Parameter objects, each with attributes 'scaling', 'lower_bound', 'upper_bound', 
+        and 'projection' specifying how to transform the corresponding scattered_mutants.
+
+    Raises
+    ------
+    ValueError
+        If an unrecognized scaling is provided.
+    """
+    cmaes_instance.transformed_mutants = np.zeros_like(cmaes_instance.scattered_mutants)
+    for i, param in enumerate(cmaes_instance._parameters):
+        if param.scaling == 'linear':
+            cmaes_instance.transformed_mutants[i] = linear_transform(cmaes_instance.scattered_mutants[i], param.lower_bound, param.upper_bound)
+        elif param.scaling == 'log':
+            cmaes_instance.transformed_mutants[i] = logarithmic_transform(cmaes_instance.scattered_mutants[i], param.lower_bound, param.upper_bound)
+        else:
+            raise ValueError('Unrecognized scaling, must be linear or log')
+        if param.projection is not None:
+            cmaes_instance.transformed_mutants[i] = np.asarray(list(map(param.projection, cmaes_instance.transformed_mutants[i])))
+
+def generate_sources(cmaes_instance):
+    """
+    Generates sources by calling the callback function on transformed data according to the set mode.
+
+    Depending on the mode, the method selects a subset of transformed mutants, possibly extending
+    it with zero-filled columns at specific positions, and then passes the processed data to the
+    callback function. The results are stored in a contiguous NumPy array in cmaes_instance.sources.
+
+    Raises
+    ------
+    ValueError
+        If an unsupported mode is provided.
+
+    Attributes
+    ----------
+    mode : str
+        A string representing the mode of operation, which can be 'mt', 'mt_dev', 'mt_dc', or 'force'.
+    transformed_mutants : np.ndarray
+        A 2D numpy array that contains the transformed data to be processed.
+    callback : callable
+        A callable that is used to process the data.
+    """
+    # Mapping between modes and respective slices or insertion positions for processing.
+    mode_to_indices = {
+        'mt': (0, 6),  # For 'mt', a slice from the first 6 elements of transformed_mutants is used.
+        'mt_dev': (0, 5, 2),  # For 'mt_dev', a zero column is inserted at position 2 after slicing the first 5 elements.
+        'mt_dc': (0, 4, 1, 2),  # For 'mt_dc', zero columns are inserted at positions 1 and 2 after slicing the first 4 elements.
+        'force': (0, 3),  # For 'force', a slice from the first 3 elements of transformed_mutants is used.
+    }
+
+    # Check the mode's validity. Raise an error if the mode is unsupported.
+    if cmaes_instance.mode not in mode_to_indices:
+        raise ValueError(f'Invalid mode: {cmaes_instance.mode}')
+
+    # Get the slice or insertion positions based on the current mode.
+    indices = mode_to_indices[cmaes_instance.mode]
+
+    # If the mode is 'mt' or 'force', take a slice from transformed_mutants and pass it to the callback.
+    if cmaes_instance.mode in ['mt', 'force']:
+        cmaes_instance.sources = np.ascontiguousarray(cmaes_instance.callback(*cmaes_instance.transformed_mutants[indices[0]:indices[1]]))
+    else:
+        # For 'mt_dev' and 'mt_dc' modes, insert zeros at specific positions after slicing transformed_mutants.
+        cmaes_instance.extended_mutants = cmaes_instance.transformed_mutants[indices[0]:indices[1]]
+        for insertion_index in indices[2:]:
+            cmaes_instance.extended_mutants = np.insert(cmaes_instance.extended_mutants, insertion_index, 0, axis=0)
+        # Pass the processed data to the callback, and save the result as a contiguous array in cmaes_instance.sources.
+        cmaes_instance.sources = np.ascontiguousarray(cmaes_instance.callback(*cmaes_instance.extended_mutants[0:6]))
+
 def scatter_mutants(cmaes_instance):
     """
     Splits and scatters the mutants across processes.
@@ -101,13 +181,18 @@ def gather_mutants(cmaes_instance):
     """
     # Printing the mutants on each process, their shapes and types for debugging purposes
     if cmaes_instance.verbose_level >= 2:
-        print(cmaes_instance.scattered_mutants, '\n', 'shape is', np.shape(cmaes_instance.scattered_mutants), '\n', 'type is', type(cmaes_instance.scattered_mutants))
+        print(f"Process {cmaes_instance.rank}: scattered_mutants = {cmaes_instance.scattered_mutants}")
+        print(f"Process {cmaes_instance.rank}: shape = {np.shape(cmaes_instance.scattered_mutants)}")
+        print(f"Process {cmaes_instance.rank}: type = {type(cmaes_instance.scattered_mutants)}")
 
     cmaes_instance.mutants = cmaes_instance.comm.gather(cmaes_instance.scattered_mutants, root=0)
     if cmaes_instance.rank == 0:
         cmaes_instance.mutants = np.concatenate(cmaes_instance.mutants, axis=1)
         if cmaes_instance.verbose_level >= 2:
-            print(cmaes_instance.mutants, '\n', 'shape is', np.shape(cmaes_instance.mutants), '\n', 'type is', type(cmaes_instance.mutants))  # DEBUG PRINT
+            print("Gathered mutants on root process:")
+            print("Mutants array:\n", cmaes_instance.mutants)
+            print("Shape of mutants array:", np.shape(cmaes_instance.mutants))
+            print("Type of mutants array:", type(cmaes_instance.mutants))
     else:
         cmaes_instance.mutants = None
 
